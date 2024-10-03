dde7fd768d369029f9407aae5cfa2770a4ec6f79
fvckDesa <lucaroma2705@gmail.com>
2024-10-03 23:40:44 +0200

    add link to go back to all projects

--- apps/web/app/projects/[projectId]/layout.tsx
+++ apps/web/app/projects/[projectId]/layout.tsx
@@ -5,6 +5,8 @@ import { CreateBtn } from "./create-btn";
 import Resources from "./resources";
 import { ResponsiveLayout } from "./responsive-layout";
 import { ReleaseBtn } from "./release-btn";
+import Link from "next/link";
+import { FolderPen } from "lucide-react";
 
 type ProjectLayoutProps = Layout & Page<{ projectId: string }>;
 
@@ -17,7 +19,13 @@ export default async function ProjectLayout({
   return (
     <div className="flex size-full flex-col">
       <header className="flex items-center justify-between border-b-2 border-border px-8 py-4 text-2xl">
-        <h1>{project.name}</h1>
+        <div className="flex items-center gap-3">
+          <Link href="/projects">
+            <FolderPen />
+          </Link>
+          <span>/</span>
+          <h1>{project.name}</h1>
+        </div>
         <div className="flex gap-4">
           <CreateBtn project={projectId} />
           <ReleaseBtn projectId={projectId} />
